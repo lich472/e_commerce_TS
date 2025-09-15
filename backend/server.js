c62a685a49375661369b44185f05b73ca0a51470
Lich Nguyen <lich472@example.com>
2025-09-16 01:28:30 +0930

    second changed in server.js

--- backend/server.js
+++ backend/server.js
@@ -39,13 +39,27 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
+
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
+
+  // Serve static files
+  app.use(express.static(frontendPath));
+
+  // SPA fallback for React Router
+  app.use((req, res, next) => {
+    if (req.path.startsWith("/api")) return next(); // skip API
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
+
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
